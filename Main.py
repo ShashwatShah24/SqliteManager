9656427b41fd2de5f0eba27746b6fe4e620c4850
ShashwatShah24 <shashwatm.shah@st.niituniversity.in>
2020-04-22 00:29:55 +0530

    3.4 commit

--- Main.py
+++ Main.py
@@ -1,5 +1,6 @@
 from flask import *
 import sqlite3
+import pandas as pd
 
 app = Flask(__name__)
 
@@ -31,11 +32,13 @@ def view():
 def saveDetails():
     column = []
     global tablename
+    global df
     tablename = request.form["tab"]
     con = sqlite3.connect(Database)
     con.row_factory = sqlite3.Row
     cur = con.cursor()
-    cur.execute("select * from {0}".format(tablename))
+    query="select * from {0}".format(tablename)
+    cur.execute(query)
     rows = cur.fetchall()
     colnames = cur.description
     for Allcolumns in colnames:
@@ -49,9 +52,18 @@ def saveDetails():
         for k in range(0, columnlength):
             Details.append(Allrow[k])
         CompleteDet.append(Details)
-		
+	
+    df=pd.DataFrame(list(rows),columns=column)
+    df.to_csv("output.csv")
+
     return render_template("view.html", tables=tables, Database=Database, column=column, rows=rows, columnlength=columnlength, CompleteDet=CompleteDet, tablename=tablename)
 
+@app.route('/return-files/')
+def return_files_tut():
+	try:
+		return send_file('output.csv', attachment_filename='output.csv')
+	except Exception as e:
+		return str(e)
 
 if __name__ == "__main__":
     app.run(host='127.0.0.1', port=9090)
